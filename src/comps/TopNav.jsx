3bcec3bc5deed50aaeb6a88f0d9748b50eb2b8f4
Sam-Lemon <theatrewhisper@gmail.com>
2024-07-19 04:42:59 -0700

    more styling

--- src/comps/TopNav.jsx
+++ src/comps/TopNav.jsx
@@ -8,19 +8,19 @@ import { Container, Navbar, Nav } from "react-bootstrap/";
 export default function TopNavBar() {
   return (
     <Navbar bg="#FFFFFF" fixed="top" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">
+      <Container className="ps-3">
+        <Navbar.Brand href="#home" className="ms-4">
           <Branding />
         </Navbar.Brand>
       </Container>
       <Container className="d-flex justify-content-end">
-        <Nav className="me-0">
+        <Nav className="me-2">
           <Nav.Link href="#projects">Projects</Nav.Link>
           <Nav.Link href="#blogs">Blogs</Nav.Link>
           <Nav.Link href="#gallery">Gallery</Nav.Link>
           <Nav.Link href="#about">About Me</Nav.Link>
         </Nav>
-        <Nav className="me-0">
+        <Nav className="me-4">
           <InstagramIcon />
           <LinkedInIcon />
           <VimeoIcon />
